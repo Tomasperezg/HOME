4ac2c1eb05bf5ee28adb6554bc0bc29033a8deec
tomas <perezgantomas@gmail.com>
2018-08-25 00:36:52 -0400

    first commit from continuation of blackbird homes

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+ini_set("mail.log", "/tmp/mail.log");
+ini_set("mail.add_x_header", TRUE);
+//variables
+$name = strip_tags(htmlspecialchars($_POST['Name']));
+$email_address = strip_tags(htmlspecialchars($_POST['Email']));
+$message = strip_tags(htmlspecialchars($_POST['Message']));
+// Create the email and send the message
+$to = 'perezgantomas@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from example... contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: donoreplytothisemail@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+if(mail($to,$email_subject,$email_body,$headers)){ echo "Mail sent!";} else{ echo "Error, check your logs."; }
+return true;
---
+++ script.js
@@ -0,0 +1,99 @@
+//this will keep the sticky navbar (not working at this moment)
+// window.onscroll= function () {
+//   myFunction()
+// };
+// var navbar = document.getElementById('NavBar');
+// var sticky = navbar.offsetTop;
+//
+// function myFunction(){
+//   if(window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   }
+//   else{
+//     navbar.classList.remove("sticky");
+//   }
+// }
+// this is a button that will scroll you to the top of the page
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// $('#myBtn').click( function(){
+//     $('html, body').animate({scrollTop: 0}, 'slow');
+// });
+
+//this is the function of the SlideShow
+var SlideIndex = 0;
+ShowSlides();
+
+function ShowSlides(){
+  var i;
+  var slides = document.getElementsByClassName('SlidePic');
+  for(i = 0 ; i < slides.length; i++ ){
+    var dots = document.getElementsByClassName("dot");
+    slides[i].style.display = "none";
+  }
+  SlideIndex++;
+  if(SlideIndex > slides.length)
+  {SlideIndex = 1}
+  for(i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+}
+  slides[SlideIndex-1].style.display = "block";
+    dots[SlideIndex-1].className += " active";
+    setTimeout(ShowSlides, 3000);
+}
+//this is jquery for text and picture fade animation
+$('.centered').hide().fadeIn(7500);
+$('#SlideShow').hide().fadeIn(5000);
+
+
+//This will animate when NavBar redirect to a link
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
+
+//for the form to work
+var form = $('#form'); //form id here
+form.submit(function(event) {
+event.preventDefault();
+var form_status = $('<div class="form_status"></div>');
+var $form = $(this);
+$.ajax({
+type: 'POST',
+url: "form.php",
+data: {
+Name: $("#name").val(),
+Email: $("#email").val(),
+Message: $("#message").val()
+},
+beforeSend: function() {
+form.append(form_status.html('Email is sending...').fadeIn());
+}
+}).done(function(data) {
+form_status.html('Email Sent!').delay(3210).fadeOut();
+});
+//delete messages when submit
+document.getElementById("name").value = "";
+document.getElementById("email").value = "";
+document.getElementById("message").value = "";
+});
